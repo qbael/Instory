4a9da43ca1e4786965186a20aae4ce6fe39ed406
Trong Nghia <nghiatrong060505@gmail.com>
2026-04-11 17:48:22 +0700

    update migrations

---
+++ backend/Instory.API/Migrations/20260411104343_AddFieldIntoPost.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Instory.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldIntoPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "allow_comment",
+                table: "posts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "comment_count",
+                table: "posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deleted_at",
+                table: "posts",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "posts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "like_count",
+                table: "posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "share_count",
+                table: "posts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "hashtagtrend",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hashtag_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<DateTime>(type: "date", nullable: false),
+                    post_count = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hashtagtrend", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_hashtagtrend_hashtags_hashtag_id",
+                        column: x => x.hashtag_id,
+                        principalTable: "hashtags",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_hashtagtrend_hashtag_id_date",
+                table: "hashtagtrend",
+                columns: new[] { "hashtag_id", "date" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "hashtagtrend");
+
+            migrationBuilder.DropColumn(
+                name: "allow_comment",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "comment_count",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "deleted_at",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "like_count",
+                table: "posts");
+
+            migrationBuilder.DropColumn(
+                name: "share_count",
+                table: "posts");
+        }
+    }
+}
