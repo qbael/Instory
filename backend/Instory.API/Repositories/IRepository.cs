1dd701018779826c8b98cff9ba3b469845daf932
qbael <baohoo10205@gmail.com>
2026-05-01 22:37:13 +0700

    remove unnecessary code

--- backend/Instory.API/Repositories/IRepository.cs
+++ backend/Instory.API/Repositories/IRepository.cs
@@ -7,8 +7,6 @@ namespace Instory.API.Repositories;
 
 public interface IRepository<T> where T : class
 {
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     Task<T?> GetByIdAsync(int id);
     Task AddAsync(T entity);
     Task AddRangeAsync(IEnumerable<T> entities);
--- backend/Instory.API/Repositories/impl/Repository.cs
+++ backend/Instory.API/Repositories/impl/Repository.cs
@@ -20,11 +20,6 @@ public class Repository<T> : IRepository<T> where T : class
         return await _dbSet.ToListAsync();
     }
 
-    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
-    {
-        return await _dbSet.Where(predicate).ToListAsync();
-    }
-
     public async Task<T?> GetByIdAsync(int id)
     {
         return await _dbSet.FindAsync(id);
