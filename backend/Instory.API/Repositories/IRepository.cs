feaf6aabb8b08a2c86fdc4e4eca27ecabd352d97
qbael <baohoo10205@gmail.com>
2026-05-02 15:30:38 +0700

    remove unnecessary code

--- backend/Instory.API/Repositories/IRepository.cs
+++ backend/Instory.API/Repositories/IRepository.cs
@@ -7,6 +7,7 @@ namespace Instory.API.Repositories;
 
 public interface IRepository<T> where T : class
 {
+    Task<IEnumerable<T>> GetAllAsync();
     Task<T?> GetByIdAsync(int id);
     Task AddAsync(T entity);
     Task AddRangeAsync(IEnumerable<T> entities);
